fd9237a3aaab73be3f47d51f2a294e5247ea5182
Jay-Cool <43534331+Jay-Cool@users.noreply.github.com>
2023-03-17 12:26:44 +1000

    Version 2
    Only Get Vehicles.js was updated to permit encoded queries for the vehicle model.

---
+++ v2/Delete Vehicle.js
@@ -0,0 +1,57 @@
+/*
+API:            Vehicles
+Name:           Delete Vehicle
+HTTP Method:    DELETE
+Relative path:  /vehicle/{vin}
+Description:    Delete selected vehicle.
+*/
+
+(function process( /*RESTAPIRequest*/ request, /*RESTAPIResponse*/ response) {
+
+    // Declare empty array for answer
+    var answer = [];
+
+    // Get the VIN from the path parameter. Generate 404 if not provided.
+    var vin = request.pathParams.vin;
+    if (!vin) {
+        response.setError(new sn_ws_err.NotFoundError('VIN not provided.'));
+        response.setStatus(404);
+        return;
+    }
+
+    // Query the Vehicle table for this VIN.
+    var vehicleTable = 'x_snc_veh_demo_vehicle';
+    var vehicleGr = new GlideRecord(vehicleTable);
+    vehicleGr.addQuery('vin', vin);
+    vehicleGr.query();
+
+    // Generate a 404 if no vehicle was found.
+    if (!vehicleGr.hasNext()) {
+        response.setError(new sn_ws_err.NotFoundError('No vehicle found.'));
+        response.setStatus(404);
+        return;
+    }
+
+    // For the vehicle found, generate a JSON object and save the vehicle data in it.
+    while (vehicleGr.next()) {
+
+        vehicleGr.deleteRecord();
+
+        var vehicleObj = {
+            "vin": vehicleGr.getValue('vin'),
+            "make": vehicleGr.getValue('make'),
+            "model": vehicleGr.getValue('model'),
+            "year": parseInt(vehicleGr.getValue('year'), 10),
+            "city": vehicleGr.getValue('city'),
+            "country": vehicleGr.getValue('country'),
+            "message": 'Vehicle deleted.'
+        };
+
+        answer.push(vehicleObj);
+
+    }
+
+    // Generate the response body.
+    response.setBody(answer);
+
+})(request, response);
